6e52a6e63c0be4ecfb54d5dc9c819a77e9dcbe87
ronchy <rongqi1949@gmail.com>
2023-07-04 23:28:05 +0800

    gitalk

---
+++ docs/docsify_updated.js
@@ -0,0 +1,7 @@
+window.$docsify = {
+    timeUpdater: {
+      text: ">最后更新时间: {docsify-updated}",
+      formatUpdated: "{YYYY}/{MM}/{DD}",
+      whereToPlace: "bottom",  // "top" or "bottom", default to "bottom"
+    },
+  };
