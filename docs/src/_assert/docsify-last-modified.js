5bde8c5bdd7244376f0aec9d49778fde28ace794
ronchy macbookAir <rongqi1949@gmail.com>
2026-02-01 01:07:09 +0800

    [fix]: 修复日期插件异步问题，使用 doneEach hook 确保元素已创建

--- docs/src/_assert/docsify-last-modified.js
+++ docs/src/_assert/docsify-last-modified.js
@@ -15,7 +15,18 @@ var install = function (hook, vm) {
     CONFIG.align = opts.align && typeof opts.align === 'string' ? opts.align : CONFIG.align;
 
     hook.beforeEach(function(html) {
+        return (html + '\n----\n' + '<div align= ' + CONFIG.align + '><span id="last-modified">' + CONFIG.preString + '加载中...</span></div>');
+    });
+
+    hook.doneEach(function() {
         var time = '{docsify-updated}';
+        var element = document.getElementById('last-modified');
+        
+        if (!element) {
+            console.error('docsify-last-modified: element #last-modified not found');
+            return;
+        }
+
         if (CONFIG.repo !== '') {
             var date_url = 'https://api.github.com/repos/' + CONFIG.repo + '/commits?per_page=1';
             
@@ -24,18 +35,29 @@ var install = function (hook, vm) {
                 var filePath = encodeURI(CONFIG.basePath + vm.route.file);
                 date_url = date_url + '&path=' + filePath;
             }
-            fetch(date_url).then((response) => {
-              return response.json();
-            }).then((commits) => {
-                if (commits.length > 0) {
-                    time = tinydate(CONFIG.dateFormat)(new Date(commits[0]['commit']['committer']['date']));
-                }
-              document.getElementById('last-modified').textContent = CONFIG.preString + time;
-            });
+            
+            fetch(date_url)
+                .then(function(response) {
+                    if (!response.ok) {
+                        throw new Error('GitHub API request failed: ' + response.status);
+                    }
+                    return response.json();
+                })
+                .then(function(commits) {
+                    if (commits && commits.length > 0) {
+                        time = tinydate(CONFIG.dateFormat)(new Date(commits[0]['commit']['committer']['date']));
+                    } else {
+                        time = '未知';
+                    }
+                    element.textContent = CONFIG.preString + time;
+                })
+                .catch(function(error) {
+                    console.error('docsify-last-modified error:', error);
+                    element.textContent = CONFIG.preString + '获取失败';
+                });
         } else {
-            document.getElementById('last-modified').textContent = CONFIG.preString + time;
+            element.textContent = CONFIG.preString + time;
         }
-        return (html + '\n----\n' + '<div align= ' + CONFIG.align + '><span id="last-modified"></span></div>');
     });
 };
 
